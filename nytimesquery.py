5579d8c8598b52bbc14293af2d445cc53e7c5a7d
Cecilia Reyes <cecilia_reyes@wgbh.org>
2013-06-25 15:09:03 -0400

    about as close as I was to reading this .py script

--- nytimesquery.py
+++ nytimesquery.py
@@ -1,13 +1,12 @@
 import urllib2
 import json
-
-
-# Get the json by making an API call to the Times 
-f = urllib2.urlopen("http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/30.json?api-key=7fffaa70d16f04f4357c4b1d0b873090:17:66946433")
+def query():	
+    # Get the json by making an API call to the Times 
+    f = urllib2.urlopen("http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/30.json?api-key=7fffaa70d16f04f4357c4b1d0b873090:17:66946433")
 
     # Store Python library into 'jsonResponse' to be fed to index.html
-jsonResponse = json.load(f)
-f.close()
+    jsonResponse = json.load(f)
+    f.close()
 
     #print jsonResponse #for debugging purposes
-print jsonResponse
+    return jsonResponse
