3839bf1a89f9cf11b24ad33ac518e5ac5f70dc4c
Cecilia Reyes <cecilia_reyes@wgbh.org>
2013-06-27 17:22:53 -0400

    oops..

--- nytimesquery.py
+++ nytimesquery.py
@@ -2,8 +2,7 @@ import urllib2
 import json
 def query():	
     # Get the json by making an API call to the Times 
-    f = urllib2.urlopen("http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/30.json?api-key=7fffaa70d16f04f4357c4b1d0b873090:17:66946433")
-
+    f = urllib2.urlopen();
     # Store Python library into 'jsonResponse' to be fed to index.html
     jsonResponse = json.load(f)
     f.close()
