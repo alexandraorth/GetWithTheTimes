ada03aceee1cd69dc070bf4c0a595a76a6d511b8
Alexandra Orth <alexandra.orth@me.com>
2013-07-15 13:20:27 -0500

    Began using Flask framework, Auto-updating articles

---
+++ webserver/nytimesquery.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#!/usr/bin/env python
+import requests
+import json
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/query')
+def query():	
+    # Get the json by making an API call to the Times 
+    f = requests.get("http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/30.json?api-key=7fffaa70d16f04f4357c4b1d0b873090:17:66946433")
+    # Store Python library into 'jsonResponse' to be fed to index.html
+    jsonResponse = f.text
+    f.close()
+    
+    return jsonResponse
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
---
+++ webserver/query.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+#!/usr/bin/env python
+
+print "I made it in"
