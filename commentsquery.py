584810e8741e48b3a3a783639a49e70d132724a2
Cecilia Reyes <cecilia_reyes@wgbh.org>
2013-07-16 16:43:06 -0400

    weird locations' response

--- commentsquery.py
+++ commentsquery.py
@@ -3,9 +3,9 @@ import json
 # from flask import Flask, render_template
 
 # app = Flask(__name__)
-api_key = "7fffaa70d16f04f4357c4b1d0b873090:17:66946433"
-offset = "20"
-
+api_key = "3617242150835f8e3b987deb6b58d404:0:66946433"
+offset = "25"
+url = "http://www.nytimes.com/2013/07/14/fashion/sex-on-campus-she-can-play-that-game-too.html"
 # @app.route("/")
 # def index():
 #     return render_template('compare.html')
@@ -14,15 +14,38 @@ offset = "20"
 
 # def query():
     # Get the json by making an API call to the Times
-f = requests.get("http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7.json?offset=" + offset + "&api-key=" + api_key)
+f = requests.get("http://api.nytimes.com/svc/community/v2/comments/url/exact-match.json?offset=" + offset + "&url=" + url + "&api-key=" + api_key)
     # Store Python library into 'jsonResponse' to be fed to compare.html
 jsonResponse = f.json()
+#j = json.loads(jsonResponse)
 
 f.close()
 
-for comments in jsonResponse:
-	print comments
-	
+i = 0
+
+for comments in jsonResponse['results']['comments']:
+	addresses = comments['location']
+	print addresses
+
+	for a in addresses:
+		response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + a + "&sensor=false")
+		jsonLocations = response.json()
+
+		coordinates = "["
+		locationCoor = ""
+		locationCoor += str(jsonLocations['results'][0]['geometry']['location']["lat"]) + ","
+		locationCoor += str(jsonLocations['results'][0]['geometry']['location']["lng"]) + "]"
+		coordinates += locationCoor
+
+		print coordinates
+
+		i = i + 1
+
+		coordinates += "]"
+
+	#print address
+
+#print jsonResponse
 
 # if __name__ == "__main__":
 #     app.run(port=8080, debug=True) 
