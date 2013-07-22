ea97c32a4185b3ebfd300093ca0fd9ce0875a733
Alexandra Orth <alexandra.orth@me.com>
2013-07-22 11:31:18 -0500

    Fixed scoping error

--- webserver/commentsAPI.py
+++ webserver/commentsAPI.py
@@ -4,11 +4,11 @@ import json
 import copy
 
 offset = 0
-# coordinates = []
+coordinates = []
 totalComments = 0
 def query(url):
 	global offset
-	coordinates = []
+	global coordinates
 
 	api_key = "3617242150835f8e3b987deb6b58d404:0:66946433"
 	locationCoor = ""
@@ -17,17 +17,16 @@ def query(url):
 	if  url[0] == "<":
 		url = url[1:-1]
 
-	print "http://api.nytimes.com/svc/community/v2/comments/url/exact-match.json?offset=" + str(offset) + "&url=" + url + "&api-key=" + api_key
 	f = requests.get("http://api.nytimes.com/svc/community/v2/comments/url/exact-match.json?offset=" + str(offset) + "&url=" + url + "&api-key=" + api_key)
 	jsonResponse = f.json()
 	f.close()
 
 	totalComments = jsonResponse['results']['totalCommentsFound']
-	print "this is the number of total comments" + str(totalComments)
 
 	for comments in jsonResponse['results']['comments']:
 		addresses = comments['location']
 		response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + addresses + "&sensor=false")
+
 		jsonLocations = response.json()
 
 		if jsonLocations['status'] == "OK":
@@ -37,7 +36,6 @@ def query(url):
 			coordinates.append(locationCoor)
 
 	offset = offset + len(jsonResponse['results']['comments']); # increments of 25, as per their API
-	print offset
 
 	if offset < totalComments: #if there are still comments left to be looked at
 		query(url) # call query() again
