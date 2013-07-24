10ed0cd5a5184e8529c77c5fcb9f4cea40b5f494
Alexandra Orth <alexandra.orth@me.com>
2013-07-24 10:59:06 -0500

    Merge changes

--- webserver/articlesAPI.py
+++ webserver/articlesAPI.py
@@ -5,8 +5,6 @@ import json
 
 def query(section):
 	section = section[1:-1]
-	print "this is the section from within python " + section
-	print "this is the request url: " + "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/" + section + "/1.json?&api-key=7fffaa70d16f04f4357c4b1d0b873090:17:66946433"
 	# Get the json by making an API call to the Times 
 	f = requests.get("http://api.nytimes.com/svc/mostpopular/v2/mostviewed/" + section + "/1.json?&api-key=7fffaa70d16f04f4357c4b1d0b873090:17:66946433")
 	# Store Python library into 'jsonResponse' to be fed to index.html
